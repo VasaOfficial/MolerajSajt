9009af07f461a87d820deb44b46b31bf9a96231a
VasaOfficial <vasapofficial@gmail.com>
2023-08-31 16:27:03 +0200

    feat: add alert for the user after submit

--- frontend/src/app/review/page.tsx
+++ frontend/src/app/review/page.tsx
@@ -20,6 +20,7 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
+import Alert from '@/components/Alert';
 
 const FormSchema = z.object({
   name: z.string().min(1).max(10),
@@ -28,6 +29,7 @@ const FormSchema = z.object({
 
 export default function ReviewForm() {
   const [rating, setRating] = useState(0);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
@@ -36,37 +38,45 @@ export default function ReviewForm() {
   const { executeRecaptcha } = useReCaptcha();
   const queryClient = useQueryClient();
 
-  const onSubmit = async (values: z.infer<typeof FormSchema>) => {
-    const token = await executeRecaptcha("form_submit");
-
-    const formData = {
-      name: values.name,
-      feedback: values.feedback,
-      rating: rating,
-      recaptchaToken: token,
-    };
-
-    const mutation = useMutation(
-      () =>
-        axios.post('/api/submit-feedback', formData, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }),
-      {
-        onSuccess: () => {
-          // Invalidate and refetch relevant queries after successful mutation
-          queryClient.invalidateQueries(['feedback']);
+  const mutation = useMutation(
+    async () => {
+      const token = await executeRecaptcha("form_submit");
+  
+      const formData = {
+        name: form.getValues("name"),
+        feedback: form.getValues("feedback"),
+        rating: rating,
+        recaptchaToken: token,
+      };
+  
+      const response = await axios.post('http://moleraj.local/backend/submit-feedback.php', formData, {
+        headers: {
+          'Content-Type': 'application/json',
         },
-      }
-    );
-
-    mutation.mutateAsync();
-
-   };
+      });
+  
+      return response.data;
+    },
+    {
+      onSuccess: () => {
+        
+        queryClient.invalidateQueries(['feedback']);
+      },
+    }
+  );
+  
+  const onSubmit = async (values: z.infer<typeof FormSchema>) => {
+    mutation.mutateAsync(); // Trigger the mutation
+    setIsSubmitted(true); 
+  
+    // Note: You can set setIsSubmitted here if you want to show the alert immediately after submission
+  };
 
   return (
     <Form {...form}>
+      {isSubmitted && (
+            <Alert />
+          )}
         <form onSubmit={form.handleSubmit(onSubmit)}>
       <ReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_CLIENT_KEY}>
         <div className="p-16 font-semibold flex flex-col justify-center items-center gap-10 bg-slate-200 rounded-md">
@@ -91,7 +101,7 @@ export default function ReviewForm() {
           />
           <FormField
             control={form.control}
-            name="name"
+            name="feedback"
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Utisak</FormLabel>
